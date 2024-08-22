cc9a130071dca09294497442b87d9c2394646a02
Faizsheik <faizsheik008@gmail.com>
2024-08-22 21:48:10 +0530

    React usestate useeffect example - arithmetic and counting without validation and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import CountNumbers from './CountNumbers.mjs';
+import ArithmeticOperations from './arithmetic operations.mjs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <CountNumbers/>
+      <ArithmeticOperations/>
+      
+
+
     </div>
   );
 }
